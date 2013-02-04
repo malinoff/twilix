24cc30815b0e6b5afbf5ddb4501b0161ad8a6c73
Malinovsky Dmitry <damalinov@gmail.com>
2013-02-04 13:25:31 +0700

    Added docstrings for the module.

--- twilix/bytestreams/ibb/stanzas.py
+++ twilix/bytestreams/ibb/stanzas.py
@@ -1,3 +1,7 @@
+"""
+Module provides stanzas which are used in In-Band Bytestreams (XEP-0047).
+"""
+
 from twilix.stanzas import Query, Message
 from twilix import errors
 from twilix import fields
@@ -6,9 +10,25 @@ from twilix.base.velement import VElement
 from twilix.bytestreams.ibb import IBB_NS
 
 class IbbQuery(Query):
+    """
+    Base class for any IBB query.
+    """
+
     elementUri = IBB_NS
 
 class OpenQuery(IbbQuery):
+    """
+    Query for initiating an in-band bytestream.
+
+    Attributes:
+        block_size -- defines the maximum size in bytes of each data chunk.
+
+        sid -- defines a unique session ID for this IBB session.
+
+        stanza_type -- defines whether the data will be sent using IQ stanzas
+                        or message stanzas.
+    """
+
     elementName = 'open'
 
     block_size = fields.StringAttr('block-size')
@@ -29,6 +49,16 @@ class OpenQuery(IbbQuery):
         return value
 
 class DataElement(VElement):
+    """
+    Class represents data element used to send data via IBB.
+
+    Attributes:
+        seq -- number that acts as a counter for data chunks sent in a
+                particular direction within the session.
+
+        sid -- ties the data chunk to particular IBB session.
+    """
+
     elementName = 'data'
     elementUri = IBB_NS
 
@@ -44,13 +74,26 @@ class DataElement(VElement):
         return value
 
 class DataQuery(DataElement, Query):
+    """
+    Query for data sending.
+    """
+
     pass
 
 class MessageDataQuery(DataElement):
+    """
+    Query for data sending vie message stanzas.
+    """
     parentClass = Message
 
 class CloseQuery(IbbQuery):
+    """
+    Query used to close IBB session.
+
+    Attributes:
+        sid -- session ID of the IBB to be closed.
+    """
+
     elementName = 'close'
 
     sid = fields.StringAttr('sid')
-
