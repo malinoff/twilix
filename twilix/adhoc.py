b5b3658cff45f15ba03f5c26d0f3dcdddfb23e98
Malinovsky Dmitry <damalinov@gmail.com>
2013-03-25 13:38:57 +0700

    Initial commit for adhoc

---
+++ twilix/adhoc.py
@@ -0,0 +1,51 @@
+class Session(object):
+
+    min_sid = 0
+
+    def __init__(self):
+        self.info = {}
+
+    def _gen_sid(self):
+        self.min_sid += 1 # just for example purposes
+        return self.min_sid
+
+    def generate_sid(self, jid):
+        if jid not in self.info:
+            return Error # already has sid
+        sid = self._gen_sid()
+        self.info[jid] = (sid, None) # maybe default lifetime?
+        return sid
+
+    def set_lifetime(self, jid, time):
+        # Should i check http://xmpp.org/extensions/xep-0050.html#impl-session
+        # if jid != from attribute return Error # access error
+        if jid not in self.info:
+            return Error # no sid to set lifetime
+        self.info[jid][1] = time
+
+    def get_info(self, jid):
+        # Should i check http://xmpp.org/extensions/xep-0050.html#impl-session
+        # if jid != from attribute return Error # access error
+        if jid not in self.info:
+            return Error # no sid
+        return self.info[jid]
+
+class BaseCommand(#VElement ?):
+
+    node = # str
+    action = # str
+    status = # str
+
+    def __init__(self, session, *args, **kwargs):
+        self.session = session
+        super(BaseCommand, self).__init__(*args, **kwargs)
+
+    def onSubmit(self, form=None):
+        pass
+
+class Command(BaseCommand):
+
+    def onSubmit(self, form):
+        
+
+    def onNextStep(self
