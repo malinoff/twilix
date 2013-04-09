619b219c0e5bf64733dcb00d03b2f80f9e8178d5
Malinovsky Dmitry <damalinov@gmail.com>
2013-04-09 14:00:07 +0700

    Some fixes

--- twilix/adhoc.py
+++ twilix/adhoc.py
@@ -1,36 +1,40 @@
-class BaseCommand(VElement):
+from twilix.base import VElement
+from twilix.base.myelement import EmptyStanza
+from twilix.stanzas import Iq, MyIq
+from twilix.errors import BadRequestException, ItemNotFoundException
+
+class BaseCommandStanza(VElement):
     elementName = 'command'
     parentClass = Iq
 
 class Command(BaseCommand):
 
-    xmlns = 'http://jabber.org/protocol/commands'
+    elementUri = 'http://jabber.org/protocol/commands'
 
     node = fields.StringAttr('node')
-    sessionid = fields.StringAttr('sessionid')
+    sessionid = fields.StringAttr('sessionid', required=False)
     action = fields.StringAttr('action', required=False)
     status = fields.StringAttr('status', required=False)
-    xml_lang = fields.StringAttr('xml:lang', required=False)
 
 
-class MyCommandQuery(CommandQuery):
+class MyCommandQuery(Command):
     parentClass = MyIQ
 
-    def anyHandler(self):
+    def setHandler(self):
         try:
             node = self.iq.command.node
         except:
-            raise # node not found, send bad request
+            raise BadRequestException
         if self.host._handlers.has_key(jid):
             if self.host._handlers[jid].has_key(node):
                 self.host._handlers[jid][node]['execute'](self)
             else:
-                raise #item not found
+                raise ItemNotFoundException
         elif self.host._handlers[''].has_key(node):
             self._handlers[''][node]['execute'](self)
         else:
-            raise #item not found
-        return EmptyStanza
+            raise ItemNotFoundException
+        return EmptyStanza()
 
 class Commands(object):
 
@@ -48,27 +52,27 @@ class Commands(object):
         if not self._handlers.has_key(jid):
             self._handlers[jid] = {}
         if self._handlers[jid].has_key(node):
-            raise #command exists
+            raise Exception('Command exists')
         else:
             self._handlers[jid][node] = {'execute':command}
 
     def delCommand(self, node, jid=''):
         if not self._handlers.has_key(jid):
-            raise # JID not found
+            raise Error(
         elif not self._handlers[jid].has_key(node):
-            raise # command not found
+            raise Exception('Command not found')
         else:
             del self._handlers[jid][node]
 
     def getCommand(self, node, jid=''):
         if not self._handlers.has_key(jid):
-            raise # JID not found
+            raise Exception('JID not found')
         elif not self._handlers.has_key(node):
-            raise # command not found
+            raise Exception('Command not found')
         else:
             return self._handlers[jid][node]
 
-class CommandPrototype(object):
+class BaseCommand(object):
     # need to set self.initial in ancestor for the first form
 
     name = None #need to be overriden in ancestor
@@ -81,13 +85,13 @@ class CommandPrototype(object):
 
     def init(self, commands):
         self._commands = commands
-        self._commands.addCommand(self.name, self.Execute, self._jid)
+        self._commands.addCommand(self.name, self.execute, self._jid)
 
     def getSessionID(self):
         self.count += 1
-        return 'cmd-%s-%d'%(self.name, self.count)
+        return 'cmd-%s-%d' % (self.name, self.count)
 
-    def Execute(self, request):
+    def execute(self, request):
         try:
             session = request.command.sessionid
         except: #no sessionid in command
@@ -99,14 +103,12 @@ class CommandPrototype(object):
         if not action:
             action = 'execute'
         if self.session.has_key(session):
-            if self.sessions[session]['jid'] == request.from_:
-                if self.sessions[session]['actions'].has_key(action):
-                    self.sessions[session]['actions'][action](request)
-                else:
-                    raise # have no such action, bad request
+            if (self.sessions[session]['jid'] == request.from_ and
+                    self.sessions[session]['actions'].has_key(action)):
+                self.sessions[session]['actions'][action](request)
             else:
-                raise # jid and sessionid dont match, bad request
+                raise BadRequestException
         elif session:
-            raise # have no session but have sessionid, bad request
+            raise BadRequestException
         else:
             self.initial[action](request)
